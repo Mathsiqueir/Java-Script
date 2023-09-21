aa4eeb1c9f9fa7e9bfcffae0081da05b1d464f8e
Mathsiqueir <matheus.spa140@gmail.com>
2023-09-20 23:25:01 -0300

    repetições

---
+++ Aula13/repetições.js
@@ -0,0 +1,24 @@
+console.log('Vai começar...')
+for (var c = 1; c <= 10; c++){
+    console.log(`passo ${c}`)
+    setTimeout(1)
+}
+console.log('FIM')
+
+
+  
+
+
+
+
+/*
+var c = 1
+do { //faz primeiro e depois testa
+    console.log(`passo ${c}`)
+    c ++
+} while (c <= 10)
+/*var c = 1
+while (c <= 6){
+    c ++
+    console.log(`passo ${c}`)
+}*/
